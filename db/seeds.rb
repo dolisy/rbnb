e89df74161fc36d5368e04df488309e1aad2ec26
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-08-21 15:15:41 +0200

    seed created - attention: booking.user might == booking.book.user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning book and booking..."
+
+Book.destroy_all
+Booking.destroy_all
+
+
+puts "creating users..."
+
+20.times do
+  User.create(email: Faker::Internet.email)
+end
+
+puts "creating books..."
+
+50.times do
+  Book.create(title: Faker::Book.title, user: User.all.order("RANDOM()").first())
+end
+
+puts "creating bookings..."
+
+10.times do
+  new_booking = Booking.create(user: User.all.order("RANDOM()").first(), book: Book.all.order("RANDOM()").first())
+end
